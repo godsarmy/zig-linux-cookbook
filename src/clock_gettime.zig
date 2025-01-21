514e180ac4de424c3e4fdef4878b1d60f8b03d6c
Walt Chen <godsarmycy@gmail.com>
2025-01-21 00:54:43 -0500

    Add example of clock_gettime

---
+++ src/clock_gettime.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+const linux = std.os.linux;
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    var start_ts: linux.timespec = undefined;
+    var end_ts: linux.timespec = undefined;
+    // fetch timestamp in the beginning
+    const start_rc = linux.clock_gettime(linux.CLOCK.MONOTONIC, &start_ts);
+    // sleep 1 second
+    std.time.sleep(1000000000);
+    const end_rc = linux.clock_gettime(linux.CLOCK.MONOTONIC, &end_ts);
+    try stdout.print("start rc: {}, end rc: {}\n", .{ start_rc, end_rc });
+    // print time diff
+    const sec_diff = end_ts.tv_sec - start_ts.tv_sec;
+    const nano_diff = end_ts.tv_nsec - start_ts.tv_nsec;
+    try stdout.print("elapsed time = {d} sec & {d} nanoseconds\n", .{ sec_diff, nano_diff });
+}
