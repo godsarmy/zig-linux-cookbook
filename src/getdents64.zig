af1ce5de00dfe0aa2a945b5cdcf20f400e253d35
Walt Chen <godsarmycy@gmail.com>
2025-01-26 13:13:36 -0500

    Add read dir comparison

--- src/getdents64.zig
+++ src/getdents64.zig
@@ -1,15 +1,17 @@
 const std = @import("std");
 
+const fs = std.fs;
 const mem = std.mem;
 const stdout = std.io.getStdOut().writer();
 const linux = std.os.linux;
 const buf_size = 1024;
 
 pub fn main() !void {
-    const tmp_dir = "/tmp";
+    // read dir by getdents64
+    const tmp_dir_path = "/tmp";
     var buf: [buf_size]u8 = undefined;
 
-    const open_rc = linux.open(tmp_dir, linux.O{}, 0);
+    const open_rc = linux.open(tmp_dir_path, linux.O{}, 0);
     const err = linux.E.init(open_rc);
     if (err != linux.E.SUCCESS) {
         try stdout.print("Error is {}\n", .{err});
@@ -22,12 +24,21 @@ pub fn main() !void {
             break;
         }
         var index: u32 = 0;
+        try stdout.print("total read: {}\n", .{nread});
         while (index < nread) {
             const entry = @as(*align(1) linux.dirent64, @ptrCast(&buf[index]));
             const name = mem.sliceTo(@as([*:0]u8, @ptrCast(&entry.name)), 0);
             try stdout.print("{} {s}\n", .{ entry.ino, name });
             index += entry.reclen;
         }
-        try stdout.print("total entries: {}\n", .{nread});
+    }
+
+    // read dir in zig native way, much easier
+    var tmp_dir = try fs.openDirAbsolute("/tmp", .{.iterate = true});
+    defer tmp_dir.close();
+
+    var iterate = tmp_dir.iterate();
+    while (try iterate.next()) |entry| {
+        try stdout.print("{s} ({})\n", .{ entry.name, entry.kind });
     }
 }
